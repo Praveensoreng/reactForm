a9e062a65758c7407c5ce8cfd0d2339789d8ef2e
Praveen Soreng <praveensoreng9@gmail.com>
2022-05-18 17:22:13 +0530

    react-form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React , {useState} from "react"
+import './App.css'
+
+function App(){
+  const [data, setData] = useState({
+    name: "",
+    department: "",
+    rating: ""
+  })
+  const {name, department,rating} = data
+  const nameHandler=(e)=>{
+    setData({...data, [e.target.name]:[e.target.value]})
+
+  }
+ 
+  const [list, setlist] = useState([])
+  const submitHandler=(e)=>{
+    e.preventDefault()
+    setlist([...list,{...data}])
+    
+   
+  }
+    
+    
+  
+  return(
+    <div className="app_container">
+     <div className="form_container">
+     <h1>Employee Feedback Form</h1>
+      <form onSubmit={submitHandler}>
+        <span>Name</span>
+        <input className="inputName" type="text"
+          name="name"
+          placeholder="enter name"
+          onChange={nameHandler}
+          value={name} />
+          <br/>
+          <br/>
+        <span>Department</span>
+        <input className="inputDepartment" type="text"
+          name="department"
+          placeholder="enter department"
+          onChange={nameHandler}
+          value={department} />
+          <br/>
+          <br/>
+        <span>Rating</span>
+        <input className="inputRating" type="number"
+          name="rating"
+          placeholder="enter rating"
+          onChange={nameHandler}
+          value={rating} />
+          <br/>
+          <br/>
+          <input className="inputSubmit" type="submit"/>
+      </form>
+     </div>
+      <section className="list_container">
+       {
+         list.map((v)=>{
+          return(
+           
+              <div className="list_item">
+               <p style={{color:'white'}}>Name: {v.name}</p>
+               <p style={{color:'white'}}>Department: {v.department}</p>
+               <p style={{color:'white'}}>Rating: {v.rating}</p>
+              </div>
+            
+          )
+         })
+       }
+      </section>
+      
+       
     </div>
   );
 }
